79f935e3739d6d18215aaec7ae635fbf8c81cecf
On-cloud7 <priyanka.p62003@gmail.com>
2024-01-10 11:05:26 +0530

    files added

--- noincreasingreversepyramid.java
+++ noincreasingreversepyramid.java
@@ -1,10 +1,12 @@
-public class increasingnopyramid {
+public class noincreasingreversepyramid {
     public static void main(String[] args) {
-        for(int i =1;i<=4;i++){
+        int n =5;
+        for(int i= n ; i>=1;i--){
             for(int j =1;j<=i;j++){
                 System.out.print( j + " ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
+
 }
