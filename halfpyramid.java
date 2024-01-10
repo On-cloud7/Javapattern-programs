4572f4b7d99421253be6bea09f487402f982ad05
On-cloud7 <priyanka.p62003@gmail.com>
2024-01-10 10:52:40 +0530

    files added

--- halfpyramid.java
+++ halfpyramid.java
@@ -1,11 +1,11 @@
-public class halfpyramid {
+public class halfpyramid{
     public static void main(String[] args) {
         int n =4;
-        for(int i=1;i<=n ;i++){
-            for(int j =1;j<= i;j++){
-                System.out.print(" *");
+        for(int i=1; i<=n ;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println("");
         }
     }
 }
